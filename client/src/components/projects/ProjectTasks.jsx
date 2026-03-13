80bd24adbd3d723565e4016dc3d0fd701787c95f
Saman Pandey <singularity.1@hotmail.com>
2026-03-13 10:20:50 +0530

    fix: Replaced all toast.dismissAll() calls with toast.dismiss()

--- client/src/components/projects/ProjectTasks.jsx
+++ client/src/components/projects/ProjectTasks.jsx
@@ -114,10 +114,10 @@ const ProjectTasks = ({ tasks }) => {
             updatedTask.status = newStatus;
             dispatch(updateTask(updatedTask));
 
-            toast.dismissAll();
+            toast.dismiss();
             toast.success('Task status updated successfully');
         } catch (error) {
-            toast.dismissAll();
+            toast.dismiss();
             toast.error(error?.response?.data?.message || error.message);
         }
     };
@@ -132,10 +132,10 @@ const ProjectTasks = ({ tasks }) => {
 
             dispatch(deleteTask(selectedTasks));
 
-            toast.dismissAll();
+            toast.dismiss();
             toast.success('Tasks deleted successfully');
         } catch (error) {
-            toast.dismissAll();
+            toast.dismiss();
             toast.error(error?.response?.data?.message || error.message);
         }
     };
